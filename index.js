2e6fe21d9ff3c8b8e772aec64c068bafd705df1d
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-07 19:02:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,38 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ function randomPrice(){
+   return Math.floor((Math.random() * 100)+1);
+ }
+ cart.push({[item]: randomPrice()});
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  
+  if(!cart.length){
+    console.log('Your shopping cart is empty.');
+    return;
+  }
+  
+  var result = 'In your cart, you have ';
+  result += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}`;
+  
+  for(var i=1; i<cart.length -1; i++){
+    result += `, ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`;
+;  }
+  
+  if(cart.length>2){
+    result += ','
+  }
+  if(cart.length > 1){
+    result += ` and ${Object.keys(cart[cart.length -1])} at $${cart[cart.length-1][Object.keys(cart[cart.length -1])]}`;
+  }
+ 
+  result += '.';
+  console.log(result);
 }
 
 function total() {
